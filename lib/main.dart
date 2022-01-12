6ffb147d1381b47a2bb5cd102d71cec6e0a6bf20
Khuram Khan <khuram2025@gmail.com>
2022-01-12 14:52:13 +0300

    10 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'splashScreen/splashScreen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
